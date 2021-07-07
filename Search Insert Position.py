fef562e7beb42db6dc6dcb0be2219c7c2d25a8e2
kondrat <85776336+drugsurfer@users.noreply.github.com>
2021-07-07 18:28:20 +0300

    Add files via upload

---
+++ Search Insert Position.py
@@ -0,0 +1,16 @@
+'''
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+'''
+def searchInsert(nums, target):
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+        if target > nums[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
