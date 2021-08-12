b74f084d70dc2fbcb5e6722f1c5025c8814d2ed3
kondrat <eugene.kondratyev09@gmail.com>
2021-08-12 11:05:10 +0300

    solution with hashMap for fourSum

--- FourSum.py
+++ FourSum.py
@@ -1,17 +1,22 @@
 def fourSum(nums: list, target: int):
-    result = []
     nums.sort()
-    for i in range(len(nums) - 2):
+    result = set()
+    hashMap = {}
+    for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
-            l, r = j + 1, len(nums) - 1
-            while l < r:
-                sum4 = nums[i] + nums[j] + nums[l] + nums[r]
-                if sum4 == target:
-                    if [nums[i], nums[j], nums[l], nums[r]] not in result:
-                        result.append([nums[i], nums[j], nums[l], nums[r]])
-                    r -= 1
-                elif sum4 > target:
-                    r -= 1
-                else:
-                    l += 1
-    return result
+            sumA = nums[i] + nums[j]
+            if sumA not in hashMap.keys():
+                hashMap[sumA] = []
+            hashMap[sumA].append((i, j))
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            sum_ = target - (nums[i] + nums[j])
+            if sum_ in hashMap.keys():
+                for item in hashMap[sum_]:
+                    k = item[0]
+                    l = item[1]
+                    if k > j:
+                        result.add((nums[i], nums[j], nums[k], nums[l]))
+    return [list(item) for item in result]
+
+print(fourSum([-3,-2,-1,0,0,1,2,3],0))
